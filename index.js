07258baba363441a3f2284c89c63bb2349f84a07
khiemk31 <khiemnd@d2t.vn>
2022-04-28 17:27:31 +0700

    day1

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+
+app.set('view engine','pug')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{
+    name :'khiem'
+  })
 })
+app.get('/users', (req, res) => {
+  res.render('users/index',{
+    users:[
+      {id:1,name:'fadfas'},
+      {id:2,name:'adfasd'},
+      {id:3,name:'d'},
+      {id:4,name:'adsfasdf'},
+      {id:5,name:'adsfasdfasd'},
+    ]
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`VD tại port : ${port}`)
