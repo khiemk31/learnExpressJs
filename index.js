9a4fad26254dbf860453eeb1b3a5eba37c0c98fb
khiemk31 <khiemnd@d2t.vn>
2022-05-05 15:50:21 +0700

    learn get post search

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
 
-app.set('view engine','pug')
-app.set('views', './views')
+const port = 3001;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+var users = [
+  { id: 1, name: 'khiem1' },
+  { id: 2, name: 'khiem2' },
+  { id: 3, name: 'luc3' },
+  { id: 4, name: 'tri4' },
+  { id: 5, name: 'cuong5' },
+  { id: 6, name: 'cuong5' },
+];
 app.get('/', (req, res) => {
-  res.render('index',{
-    name :'khiem'
-  })
-})
-app.get('/users', (req, res) => {
-  res.render('users/index',{
-    users:[
-      {id:1,name:'fadfas',},
-      {id:2,name:'adfasd'},
-      {id:3,name:'d'},
-      {id:4,name:'adsfasdf'},
-      {id:5,name:'adsfasdfasd'},
-    ]
-  })
-})
+  res.render('index', {
+    name: 'khiem',
+  });
+});
 
-app.get('/user/search')
+app.get('/users', (req, res) => {
+  res.render('users/index', {
+    users: users,
+  });
+});
+
+app.get('/users/search', (req, res) => {
+  var q = req.query.q;
+  var matchedUsers = users.filter((user) => {
+    return user.name.toLowerCase.indexOf(q.toLowerCase) != -1;
+  });
+  res.render('users/index', {
+    users: matchedUsers,
+  });
+});
+
+app.get('/users/create', (req, res) => {
+  res.render('users/create');
+});
+
+app.post('/users/create' ,(req,res)=>{
+  users.push(req.body);
+  res.redirect('/users');
+});
 
 app.listen(port, () => {
-  console.log(`VD tại port : ${port}`)
-})
+  console.log(`khởi tạo cổng : ${port}`);
+});
