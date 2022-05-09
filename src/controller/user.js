d2d10655935ebc91b6621b933a2d0b0101405972
khiemk31 <khiemk31@gmail.com>
2022-05-09 23:18:32 +0700

    no message

--- src/controller/user.js
+++ src/controller/user.js
@@ -62,8 +62,7 @@ const login = async (req, res) => {
       return res.status(404).json({message: 'Người dùng không tồn tại'});
     } else {
       const token = await comparePassword(user[0], password);
-      console.log(token);
-      return res.status(200).json({message: ['Đăng nhập thành công'], token});
+      return res.status(200).json({token, message: 'Đăng nhập thành công'}, token);
     }
   } catch (e) {
     return res.status(500).json({message: `${e}`});
